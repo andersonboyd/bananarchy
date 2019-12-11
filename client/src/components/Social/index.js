a3617241808f1d5f2e149c11551fd1bda90d21a3
Anderson <andersonswaimboyd@gmail.com>
2019-12-11 14:07:12 -0600

    colored social links

--- client/src/components/Social/index.js
+++ client/src/components/Social/index.js
@@ -2,21 +2,25 @@ import React, { Component } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "../Social/style.css";
 
+const div = () => {
+    return <div></div>
+}
+
 class Social extends Component {
     render(){
         return(
             <div className="social-box">
                 <div className="link">
-                    <a href="http://facebook.com/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={["fab", "facebook"]} /></a>
+                    <a id="fb-icon" href="http://facebook.com/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={["fab", "facebook"]} /></a>
+                </div>
+                <div className="link" id="ig">
+                    <a as={div} id="ig-icon" href="http://instagram.com/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon className="fa-gradient" icon={["fab", "instagram"]} /></a>
                 </div>
                 <div className="link">
-                    <a href="http://instagram.com/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={["fab", "instagram"]} /></a>
+                    <a id="tw-icon" href="http://twitter.com/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={["fab", "twitter"]} /></a>
                 </div>
                 <div className="link">
-                    <a href="http://twitter.com/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={["fab", "twitter"]} /></a>
-                </div>
-                <div className="link">
-                    <a href="http://yelp.com/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={["fab", "yelp"]} /></a>
+                    <a id="yp-icon" href="http://yelp.com/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={["fab", "yelp"]} /></a>
                 </div>
             </div>
         )
